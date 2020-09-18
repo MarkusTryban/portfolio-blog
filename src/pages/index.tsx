085bd45d3fa33fd58b88f4061586c6126069156d
MarkusTryban <markustryban@gmail.com>
2020-09-18 17:46:34 -0400

    App formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react';
 
 const Home = () => {
-  return <div>Hello world!</div>
-}
+  return <div>Hello world!</div>;
+};
 
-export default Home
+export default Home;
