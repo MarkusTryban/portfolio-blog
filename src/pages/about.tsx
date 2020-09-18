90d05c02d50c9976ebb39dfb8b3e559b939eed5f
MarkusTryban <markustryban@gmail.com>
2020-09-18 19:57:38 -0400

    Return h1 from About

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = (): JSX.Element => {
+  return (
+    <div style={{ color: `teal` }}>
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  );
+};
+
+export default About;
