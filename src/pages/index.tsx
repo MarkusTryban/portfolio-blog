1ba1dca3e09eec43d36752351cbe33119b4c57d3
MarkusTryban <markustryban@gmail.com>
2020-09-18 19:46:06 -0400

    Return h1 & random img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src='https://source.unsplash.com/random/400x200' alt='' />
+    </div>
+  );
 };
 
 export default Home;
