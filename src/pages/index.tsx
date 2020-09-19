1f19566b0f380da657af8d9a6edefc67d261691a
MarkusTryban <markustryban@gmail.com>
2020-09-18 20:21:13 -0400

    Return Link to contact page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { Link } from 'gatsby';
+
+import Header from '../components/header';
+
 const Home = (): JSX.Element => {
   return (
     <div style={{ color: `purple` }}>
-      <h1>Hello Gatsby!</h1>
+      <Link to='/contact/'>Contact</Link>
+      <Header headerText='Hello Gatsby' />
       <p>What a world.</p>
       <img src='https://source.unsplash.com/random/400x200' alt='' />
     </div>
