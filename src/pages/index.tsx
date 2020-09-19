1be90ce0ca9d3e47ca28e11e8e9706add5ce0705
MarkusTryban <markustryban@gmail.com>
2020-09-19 16:17:30 -0400

    Add Layout to Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,18 @@ import { Link } from 'gatsby';
 
 import Header from '../components/header';
 
+import Layout from '../components/layout';
+
 const Home = (): JSX.Element => {
   return (
-    <div style={{ margin: '3rem auto', maxWidth: 600 }}>
+    <Layout>
       <Link to='/contact/'>Contact</Link>
       <br />
       <Link to='/about/'>About</Link>
       <Header headerText='Hello Gatsby' />
       <p>What a world.</p>
       <img src='https://source.unsplash.com/random/400x200' alt='' />
-    </div>
+    </Layout>
   );
 };
 
