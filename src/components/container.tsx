5a041482ac35e74f06f7adadcdf91af58cfdae3e
MarkusTryban <markustryban@gmail.com>
2020-09-19 16:27:39 -0400

    Remove container css

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import containerStyles from './container.module.css';
-
 const Container = ({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element => {
-  return <div className={containerStyles.container}>{children}</div>;
+  return <div>{children}</div>;
 };
 
 export default Container;
