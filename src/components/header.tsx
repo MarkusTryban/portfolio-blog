7807caff92d1b78c637d99b6d109c10bfc3d7b35
MarkusTryban <markustryban@gmail.com>
2020-09-20 11:29:32 -0400

    Refactor class to functional component with hooks

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,60 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './header.css';
 
-export default class Header extends Component {
-  constructor(props: Record<string, unknown>) {
-    super(props);
-    this.state = {
-      openMenu: false,
-    };
+const Header = (): JSX.Element => {
+  const [openMenu, setOpenMenu] = useState(false);
 
-    this.toggleMenu = this.toggleMenu.bind(this);
-  }
-
-  toggleMenu = (value: any) => {
-    this.setState({ openMenu: value });
+  const toggleMenu = (value: boolean) => {
+    setOpenMenu(value);
   };
 
-  render(): JSX.Element {
-    const { openMenu } = this.state;
+  return (
+    <>
+      <a
+        onClick={(_) => toggleMenu(!openMenu)}
+        className={`menu-toggle rounded ${openMenu ? 'active' : ''}`}
+        href='/#'
+      >
+        {openMenu ? (
+          <i className='fas fa-times' />
+        ) : (
+          <i className='fas fa-bars' />
+        )}
+      </a>
+      <nav id='sidebar-wrapper' className={` ${openMenu ? 'active' : ''}`}>
+        <ul className='sidebar-nav'>
+          <li className='sidebar-brand'>
+            <a href='#page-top'>Gatsby Starter</a>
+          </li>
+          <li className='sidebar-nav-item'>
+            <a href='#page-top'>Home</a>
+          </li>
+          <li className='sidebar-nav-item'>
+            <a href='#about'>About</a>
+          </li>
+          <li className='sidebar-nav-item'>
+            <a href='#services'>Services</a>
+          </li>
+          <li className='sidebar-nav-item'>
+            <a href='#portfolio'>Portfolio</a>
+          </li>
+          <li className='sidebar-nav-item'>
+            <a href='#contact'>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-    return (
-      <>
-        <a
-          onClick={(_) => this.toggleMenu(!openMenu)}
-          className={`menu-toggle rounded ${openMenu ? 'active' : ''}`}
-          href='/#'
-        >
-          {openMenu ? (
-            <i className='fas fa-times' />
-          ) : (
-            <i className='fas fa-bars' />
-          )}
-        </a>
-        <nav id='sidebar-wrapper' className={` ${openMenu ? 'active' : ''}`}>
-          <ul className='sidebar-nav'>
-            <li className='sidebar-brand'>
-              <a href='#page-top'>Gatsby Starter</a>
-            </li>
-            <li className='sidebar-nav-item'>
-              <a href='#page-top'>Home</a>
-            </li>
-            <li className='sidebar-nav-item'>
-              <a href='#about'>About</a>
-            </li>
-            <li className='sidebar-nav-item'>
-              <a href='#services'>Services</a>
-            </li>
-            <li className='sidebar-nav-item'>
-              <a href='#portfolio'>Portfolio</a>
-            </li>
-            <li className='sidebar-nav-item'>
-              <a href='#contact'>Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </>
-    );
-  }
-}
+export default Header;
