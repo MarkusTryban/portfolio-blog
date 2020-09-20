e85d71a1a24f291f1c71b4eff2cb47ee99bc1b27
MarkusTryban <markustryban@gmail.com>
2020-09-19 21:06:56 -0400

    Return Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { Link } from 'gatsby';
-
 import Header from '../components/header';
 
 import Layout from '../components/layout';
@@ -9,12 +7,7 @@ import Layout from '../components/layout';
 const Home = (): JSX.Element => {
   return (
     <Layout>
-      <Link to='/contact/'>Contact</Link>
-      <br />
-      <Link to='/about/'>About</Link>
       <Header headerText='Hello Gatsby' />
-      <p>What a world.</p>
-      <img src='https://source.unsplash.com/random/400x200' alt='' />
     </Layout>
   );
 };
