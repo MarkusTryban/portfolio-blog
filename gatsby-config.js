d5b1e8e469bb2a298a232e4f03da863117c57235
MarkusTryban <markustryban@gmail.com>
2020-09-19 22:34:07 -0400

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  plugins: [
-    'gatsby-plugin-typescript',
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography',
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-typescript'],
 };
