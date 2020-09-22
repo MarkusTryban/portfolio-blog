4f14381fe318e5458f309861fa7df841b5443c5e
MarkusTryban <markustryban@gmail.com>
2020-09-21 21:52:04 -0400

    Create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = (): JSX.Element => {
+  return (
+    <footer className='footer text-center'>
+      <div className='container'>
+        <p className='text-muted small mb-0'>Copyright &copy; Gatsby Starter</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
