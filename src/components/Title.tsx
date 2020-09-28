d2970ce3859b89d4d170b734626e751961e3c696
MarkusTryban <markustryban@gmail.com>
2020-09-28 16:48:59 -0400

    Create Title for main contnet

---
+++ src/components/Title.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Helmet } from 'react-helmet';
+
+const Title = (): JSX.Element => {
+  return (
+    <Helmet>
+      <title>Passionate Software Engineer</title>
+    </Helmet>
+  );
+};
+
+export default Title;
