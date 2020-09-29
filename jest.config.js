bfaa5d058777c8e5f67406adddb880611638c6ee
MarkusTryban <markustryban@gmail.com>
2020-09-29 16:18:21 -0400

    Cleanup jest config

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,6 @@ module.exports = {
     '\\.svg': `<rootDir>/svgTransform.js`,
     'typeface-*': 'identity-obj-proxy',
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mocks.js`,
   },
   testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`, `\\.svg`],
