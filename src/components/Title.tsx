73bc4ae43dffd258e830cf3784c262a9e161371c
MarkusTryban <markustryban@gmail.com>
2020-09-29 19:11:05 -0400

    Cleanup favicon imort

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import { Helmet } from 'react-helmet';
 
+import favicon from '../assets/favicon/favicon.ico';
+
 const Title = (): JSX.Element => {
   return (
     <>
       <Helmet>
         <meta charSet='utf-8' />
         <title>Passionate Software Engineer</title>
-        <link rel='icon' href={'../assets/favicon/favicon.ico'} />
+        <link rel='icon' href={favicon} />
       </Helmet>
     </>
   );
