57afb0bb7e277c640fdb30e850d9603a762f196b
MarkusTryban <markustryban@gmail.com>
2020-09-29 16:17:25 -0400

    Move favicon

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import favicon from '../assets/favicon/favicon.ico';
-
 import { Helmet } from 'react-helmet';
 
 const Title = (): JSX.Element => {
@@ -10,7 +8,7 @@ const Title = (): JSX.Element => {
       <Helmet>
         <meta charSet='utf-8' />
         <title>Passionate Software Engineer</title>
-        <link rel='icon' href={favicon} />
+        <link rel='icon' href={'../assets/favicon/favicon.ico'} />
       </Helmet>
     </>
   );
