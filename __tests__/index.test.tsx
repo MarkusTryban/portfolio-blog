4e4a854554a7c85fbc97e5e29416f827804db157
MarkusTryban <markustryban@gmail.com>
2020-09-29 18:47:30 -0400

    Should return Portfolio link

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -7,3 +7,8 @@ import Home from '../src/pages/index';
 test('should render without crashing', () => {
   render(<Home />);
 });
+
+test('should return Portfolio link', () => {
+  render(<Home />);
+  expect(screen.getByTitle('Portfolio btn'));
+});
